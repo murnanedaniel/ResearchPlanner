29bf6eec790a1c36f88be2c35c92dbd135a0a90e
Daniel Murnane <danieltmurnane@gmail.com>
2024-11-14 10:28:29 +0100

    Fix heroku deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone'
-  }
-  
-  export default nextConfig
+    output: 'standalone',
+    experimental: {
+        // This will allow better optimization
+        optimizeCss: true,
+        // Helps with module resolution
+        esmExternals: 'loose'
+    },
+    // Add this to help with MDX editor
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false, path: false };
+        return config;
+    }
+}
+
+module.exports = nextConfig
